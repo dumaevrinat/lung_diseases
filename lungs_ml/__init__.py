6aca50f5656c2a6f77c9cd86527d1cda7e6ad3a1
dumaevrinat <dumaevrinat@gmail.com>
2021-04-04 23:24:04 +0300

    add prediction service to lungs_ml

--- lungs_ml/__init__.py
+++ lungs_ml/__init__.py
@@ -1,2 +1,3 @@
 from .image_processing import *
 from .models import *
+from .prediction_service import *
--- utils/constants.py
+++
@@ -1,9 +0,0 @@
-CLASSIFICATION_MODEL_WEIGHTS_PATH = '../prediction_models/classification_model.hdf5'
-CLASSIFICATION_MODEL_CONFIG_PATH = '../prediction_models/classification_model_structure.json'
-
-UNET_MODEL_WEIGHTS_PATH = '../prediction_models/unet_model.hdf5'
-UNET_MODEL_CONFIG_PATH = '../prediction_models/unet_model_structure.json'
-
-BOT_TOKEN = '1646455756:AAE3ao97o4YIadDcaQF2pwoGDaxHA95uDB0'
-
-FONT_PATH = 'assets/Roboto.ttf'
